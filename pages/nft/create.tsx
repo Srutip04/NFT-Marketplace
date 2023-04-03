74796951b01900ba73fe8f28fe2ef5091dec61e1
Srutip04 <subuhishruti75@gmail.com>
2023-04-03 23:25:39 +0530

    links and create page

---
+++ pages/nft/create.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next";
+import { BaseLayout } from "../../components";
+
+const NftCreate: NextPage = () => {
+  return <BaseLayout>Nft Create</BaseLayout>;
+};
+
+export default NftCreate;
