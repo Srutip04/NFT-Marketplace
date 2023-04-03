1cf86940abb475f73a8bd6adaf4ad4b1a85f3991
Srutip04 <subuhishruti75@gmail.com>
2023-04-03 23:11:24 +0530

    Display nft item values

--- components/nft/item/index.tsx
+++ components/nft/item/index.tsx
@@ -1,16 +1,20 @@
 /* eslint-disable @next/next/no-img-element */
 
 import { FunctionComponent } from "react";
+import { NftMeta } from "../../../types/nft";
 
-const NftItem: FunctionComponent = () => {
+type NftItemProps = {
+  item: NftMeta;
+};
+
+const NftItem: FunctionComponent<NftItemProps> = ({item}) => {
+  
   return (
     <>
       <div className="flex-shrink-0">
         <img
           className={`h-full w-full object-cover`}
-          src={
-            "https://eincode.mypinata.cloud/ipfs/QmaQYCrX9Fg2kGijqapTYgpMXV7QPPzMwGrSRfV9TvTsfM/Creature_1.png"
-          }
+          src={item.image}
           alt="New NFT"
         />
       </div>
@@ -18,11 +22,9 @@ const NftItem: FunctionComponent = () => {
         <div className="flex-1">
           <p className="text-sm font-medium text-indigo-600">Creatures NFT</p>
           <div className="block mt-2">
-            <p className="text-xl font-semibold text-gray-900">
-              Eincode Creature #1
-            </p>
+            <p className="text-xl font-semibold text-gray-900">{item.name}</p>
             <p className="mt-3 mb-3 text-base text-gray-500">
-              Fierce violet creature. Very durable and tanky.
+              {item.description}
             </p>
           </div>
         </div>
@@ -35,27 +37,27 @@ const NftItem: FunctionComponent = () => {
               <dd className="order-1 text-xl font-extrabold text-indigo-600">
                 <div className="flex justify-center items-center">
                   100
-                  {/* <img className="h-6" src="/images/small-eth.webp"/> */}
-                  ETH
+                  <img
+                    className="h-6"
+                    src="/images/small-eth.webp"
+                    alt="ether icon"
+                  />
                 </div>
               </dd>
             </div>
-            <div className="flex flex-col px-4 pt-4">
-              <dt className="order-2 text-sm font-medium text-gray-500">
-                Health
-              </dt>
-              <dd className="order-1 text-xl font-extrabold text-indigo-600">
-                100
-              </dd>
-            </div>
-            <div className="flex flex-col px-4 pt-4">
-              <dt className="order-2 text-sm font-medium text-gray-500">
-                Attack
-              </dt>
-              <dd className="order-1 text-xl font-extrabold text-indigo-600">
-                40
-              </dd>
-            </div>
+            {item.attributes.map((attribute) => (
+              <div
+                key={attribute.trait_type}
+                className="flex flex-col px-4 pt-4"
+              >
+                <dt className="order-2 text-sm font-medium text-gray-500">
+                  {attribute.trait_type}
+                </dt>
+                <dd className="order-1 text-xl font-extrabold text-indigo-600">
+                  {attribute.value}
+                </dd>
+              </div>
+            ))}
           </dl>
         </div>
         <div>
