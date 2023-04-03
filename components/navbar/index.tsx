6361fea21170d4e012482cc4631c24a57ce1f5ae
Srutip04 <subuhishruti75@gmail.com>
2023-04-03 23:36:32 +0530

    profile link

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -84,41 +84,16 @@ export default function Navbar() {
                   <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
                     <Menu.Item>
                       {({ active }) => (
-                        <a
-                          href="#"
-                          className={classNames(
-                            active ? "bg-gray-100" : "",
-                            "block px-4 py-2 text-sm text-gray-700"
-                          )}
-                        >
-                          Your Profile
-                        </a>
-                      )}
-                    </Menu.Item>
-                    <Menu.Item>
-                      {({ active }) => (
-                        <a
-                          href="#"
-                          className={classNames(
-                            active ? "bg-gray-100" : "",
-                            "block px-4 py-2 text-sm text-gray-700"
-                          )}
-                        >
-                          Settings
-                        </a>
-                      )}
-                    </Menu.Item>
-                    <Menu.Item>
-                      {({ active }) => (
-                        <a
-                          href="#"
-                          className={classNames(
-                            active ? "bg-gray-100" : "",
-                            "block px-4 py-2 text-sm text-gray-700"
-                          )}
-                        >
-                          Sign out
-                        </a>
+                        <Link legacyBehavior href="/profile">
+                          <a
+                            className={classNames(
+                              active ? "bg-gray-100" : "",
+                              "block px-4 py-2 text-sm text-gray-700"
+                            )}
+                          >
+                            Your Profile
+                          </a>
+                        </Link>
                       )}
                     </Menu.Item>
                   </Menu.Items>
---
+++ pages/profile.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next";
+import { BaseLayout } from "../components";
+
+const Profile: NextPage = () => {
+  return <BaseLayout>Profile</BaseLayout>;
+};
+
+export default Profile;
